8c152115d8a4e9a566a9e6078ff067e810bb2f05
Axely7 <axely7@hotmail.com>
2021-08-16 19:24:20 -0500

    Creating users and connecting data base

---
+++ db/resolvers.js
@@ -0,0 +1,36 @@
+const Usuario = require('../models/Usuario')
+
+// Resolvers
+const resolvers={
+    Query:{
+            obtenerCurso: () => "Algo"
+    },
+    Mutation:{
+        nuevoUsuario: async (_, { input }) => {
+
+            const {email, password} = input;
+
+            //Revisar si el usuario ya esta registrado
+            const existeUsuario = await Usuario.findOne({email});
+            console.log(existeUsuario);
+            if(existeUsuario){
+                throw new Error('El usuario ya está registrado');
+            }
+            //Hashear su password
+
+            
+            try{
+                //Guardarlo en la base de datos
+                const usuario = new Usuario(input);
+                usuario.saver();
+                return usuario;
+
+            }catch(error){
+                console.log(error);
+            }
+        }
+    }
+        
+}
+
+module.exports  = resolvers;
---
+++ models/Usuario.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UsuariosSchema = mongoose.Schema({
+    //Esto es lo que se creará en la base de datos.
+    nombre:{
+        type: String,
+        required: true,
+        trim: true //Elimina los espacios por error que se podrían 
+        //incluir en el formulario
+    },
+    apellido:{
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    creado:{
+        type: Date,
+        default: Date.now()
+    }
+    //Más adelante podemos agregar más campos.
+
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
