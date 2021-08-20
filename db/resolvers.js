4476dcac88faedc6ad1579ecc12c3b7e451e0d38
Axely7 <axely7@hotmail.com>
2021-08-19 23:51:31 -0500

    Creating token

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,5 +1,15 @@
 const Usuario = require('../models/Usuario');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path: 'variables.env'});
+
+const crearToken = (usuario, secreta, expiresIn) => {
+    //console.log(usuario);
+    const {id, email, nombre, apellido} = usuario;
+
+    return jwt.sign({id, email, nombre, apellido,}, secreta, {expiresIn})
+}
+
 
 // Resolvers
 const resolvers={
@@ -37,15 +47,21 @@ const resolvers={
             const {email, password} = input;
 
             //Si el usuario existe
-            const existUsuario = await Usuario.findOne({email});
+            const existeUsuario = await Usuario.findOne({email});
             if(!existeUsuario){
                 throw new Error('El usuario no existe');
             }
 
             //Revisar si el password es correcto
-
+            const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password);
+            if(!passwordCorrecto){
+                throw new Error('El password es incorrecto');
+            }
 
             //Crear el token
+            return{
+                token: crearToken(existeUsuario, process.env.SECRETA, '24h') /*Al día siguiente tendrían que iniciar sesión*/
+            }
         }
     }
         
