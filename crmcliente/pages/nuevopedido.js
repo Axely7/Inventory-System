5559acfd5b801483e3771afa8168bbf57bbc1485
Axely7 <axely7@hotmail.com>
2021-09-09 12:33:24 -0500

    importin react-select

---
+++ crmcliente/pages/nuevopedido.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout';
+import Select from 'react-select';
+
+const NuevoPedidooooo = () =>{
+    return(
+        <Layout>
+            <h1 className="text-2xl text-gray-80 font-light">Crear Nuevo Pedido</h1>
+        </Layout>
+        
+    );
+}
+
+export default NuevoPedidooooo;
