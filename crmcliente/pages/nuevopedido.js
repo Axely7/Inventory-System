4b21b7a7be88991547b3e0f6d432e9dc53f00532
Axely7 <axely7@hotmail.com>
2021-09-10 13:10:42 -0500

    using react select

--- crmcliente/pages/nuevopedido.js
+++ crmcliente/pages/nuevopedido.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../components/Layout';
 import Select from 'react-select';
+const options = [
+    { id: 'chocolate', nombre: 'Chocolate' },
+    { id: 'strawberry', nombre: 'Strawberry' },
+    { id: 'vanilla', nombre: 'Vanilla' }
+  ]
 
-const NuevoPedidooooo = () =>{
+
+const NuevoPedido = () =>{
+
+    const [sabores, setSabores] = useState([]);
+
+    useEffect(() =>{
+        console.log(sabores);
+    }, [sabores]);
+
+    const seleccionarSabor = sabores =>{
+        setSabores(sabores)
+    }
+
+
     return(
         <Layout>
             <h1 className="text-2xl text-gray-80 font-light">Crear Nuevo Pedido</h1>
+            <Select options={options}
+            isMulti={true}
+            onChange={opcion => seleccionarSabor(opcion)}
+            getOptionValue = {(opciones) => opciones.id}
+            getOptionLabel = {(opciones) => opciones.nombre}
+            placeholder = "Seleccione el sabor"
+            noOptionsMessage = {() => "No hay resultados"}
+            />
         </Layout>
+        
         
     );
 }
 
-export default NuevoPedidooooo;
+export default NuevoPedido;
