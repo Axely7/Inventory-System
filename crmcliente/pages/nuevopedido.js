941d1e1ff796760e7e6be72846dd0ea0a15fc202
Axely7 <axely7@hotmail.com>
2021-09-13 12:26:41 -0500

    adding quantity products

--- crmcliente/pages/nuevopedido.js
+++ crmcliente/pages/nuevopedido.js
@@ -2,6 +2,7 @@ import React, {useContext} from 'react';
 import Layout from '../components/Layout';
 import AsignarCliente from '../components/pedidos/AsignarCliente'
 import AsignarProductos from '../components/pedidos/AsignarProductos'
+import ResumenPedido from '../components/pedidos/ResumenPedido';
 // Context de pedidos
 import PedidoContext from '../context/pedidos/PedidoContext'
 
@@ -15,8 +16,13 @@ const NuevoPedido = () =>{
     return(
         <Layout>
             <h1 className="text-2xl text-gray-80 font-light">Crear Nuevo Pedido</h1>
-            <AsignarCliente></AsignarCliente>
-            <AsignarProductos></AsignarProductos>
+            <div className="flex justify-center mt-5">
+                <div className="w-full max-w-lg">
+                    <AsignarCliente></AsignarCliente>
+                    <AsignarProductos></AsignarProductos>
+                    <ResumenPedido></ResumenPedido>
+                </div>
+            </div>
         </Layout>
         
         
