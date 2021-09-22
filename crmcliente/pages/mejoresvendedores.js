f126c4e68ed7ef554215bcfe33544e2af5321445
Axely7 <axely7@hotmail.com>
2021-09-22 11:59:22 -0500

    best sellers graph done

--- crmcliente/pages/mejoresvendedores.js
+++ crmcliente/pages/mejoresvendedores.js
@@ -1,55 +1,50 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from '../components/Layout';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import {gql, useQuery} from '@apollo/client';
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-  
+const MEJORES_VENDEDORES = gql`
+  query mejoresVendedores{
+    mejoresVendedores{
+      vendedor{
+        nombre
+        email
+      }
+      total
+    }
+  }
+`;
+
 
 const MejoresVendedores = () =>{
+    const {data, loading, error, startPolling, stopPolling} = useQuery(MEJORES_VENDEDORES);
+
+    // Cuando detecta un cambio en la base de datos, pasa un segundo
+    // y ejecuta los cambios en la gráfica
+    useEffect(() => {
+      startPolling(1000);
+      return () =>{
+        stopPolling();
+      }
+    }, [startPolling, stopPolling])
+
+    if(loading) return 'cargando...';
+
+    console.log(data);
+
+    const {mejoresVendedores} = data;
+
+    const vendedorGrafica = [];
+
+    mejoresVendedores.map((vendedor, index) => {
+      vendedorGrafica[index] = {
+        ...vendedor.vendedor[0],
+        Total: vendedor.total
+      }
+    })
+
+    console.log(vendedorGrafica);
+
     return(
         <Layout>
             <h1 className="text-2xl text-gray-80 font-light">Mejores Vendedores</h1>
@@ -58,7 +53,7 @@ const MejoresVendedores = () =>{
                 className="mt-10"
                 width={600}
                 height={500}
-                data={data}
+                data={vendedorGrafica}
                 margin={{
                     top: 5,
                     right: 30,
@@ -67,11 +62,11 @@ const MejoresVendedores = () =>{
                 }}
                 >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="nombre" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="pv" fill="#3182CE" />
+                <Bar dataKey="Total" fill="#3182CE" />
                 
         </BarChart>
         </Layout>
